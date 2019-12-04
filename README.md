cc4d04c3a61dd7c34490ad844034d6027c2b3eec
Verrrtigo <karolinauskas@gmail.com>
2019-12-04 23:37:55 +0200

    Challenge solution

--- README.md
+++ README.md
@@ -21,3 +21,8 @@ You need to have `node` and `npm` installed on your machine.
 1. All test cases that are currently in `src/index.test.ts` must pass.
 1. Skip the word "and" for larger numbers, i.e. `two thousand one` instead of `two thousand and one`.
 1. The implementation must cover cases from `-99999` to `99999`.
+
+### Comments after task completion:
+Input number is split into three digit groups, which are then handled and converted to text using a separate function, which keeps getting called as long as there are three digit groups remaining after each iteration. The three digit group is further split and each digit (hundreds, tens and ones place) is found and then compared against the Map structure which holds the number-word key-value pairs. Results of the conversion are recombined to form a complete string of the input number converted to words. Prefix "negative" is added for negative values.
+
+Usability can be further extended to larger numbers by updating the Map with larger scale numbers (billion, trillion and so on).
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,65 @@
-export function convertNumberToEnglishText(n: number): string {
-    return 'not implemented';
+export function convertNumberToEnglishText(num: number): string {
+    if(num == 0) return 'zero';
+    const negative = num < 0 ? true : false;
+    let result = '';
+    num = Math.abs(num);
+    
+    for(let i = 1; Math.floor(num / i) > 0; i *= 1000) {
+        result = `${convertDigitGroupToWords(Math.floor(num / i) % 1000)} ${i > 1 ? numbersMap.get(i) : ''} ${result}`
+    }
+
+    return negative ? `negative ${result}`.trim() : result.trim();
+}
+
+const numbersMap = new Map([
+    [0, ''],
+    [1, 'one'],
+    [2, 'two'],
+    [3, 'three'],
+    [4, 'four'],
+    [5, 'five'],
+    [6, 'six'],
+    [7, 'seven'],
+    [8, 'eight'],
+    [9, 'nine'],
+    [10, 'ten'],
+    [11, 'eleven'],
+    [12, 'twelve'],
+    [13, 'thirteen'],
+    [14, 'fourteen'],
+    [15, 'fifteen'],
+    [16, 'sixteen'],
+    [17, 'seventeen'],
+    [18, 'eighteen'],
+    [19, 'nineteen'],
+    [20, 'twenty'],
+    [30, 'thirty'],
+    [40, 'forty'],
+    [50, 'fifty'],
+    [60, 'sixty'],
+    [70, 'seventy'],
+    [80, 'eighty'],
+    [90, 'ninety'],
+    [100, 'hundred'],
+    [1000, 'thousand'],
+    // [1000000, 'million'],
+    // [1000000000, 'billion'],
+    // [1000000000000, 'trillion'],
+])
+
+function convertDigitGroupToWords(num: number): string {
+    let result = ''
+    
+    if(Math.floor(num / 100)){
+        result += ` ${numbersMap.get(Math.floor(num / 100))} hundred`
+    }
+    
+    num = Math.floor(num % 100)
+    
+    if(num > 20) {
+        result += ` ${numbersMap.get(Math.floor(num - num % 10))}`
+        num = num % 10
+    }
+    
+    return `${result} ${numbersMap.get(num)}`.trim()
 }
